ae2ccb01477bc9670ea2837197c0159a2302f608
torrieri <andreastorrieri3@gmail.com>
2022-05-20 20:34:16 -0300

    finalizar exercício

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿int contador , numero;
+
+Console.WriteLine("ola eu sou a calculadora de tabuada !!!");
+Thread.Sleep(2000);
+
+Console.WriteLine("digite um numero de 0 a 10");
+numero = Convert.ToInt32(Console.ReadLine()!);
+
+contador = 0;
+
+if(numero < 0 || numero > 10){
+    Console.WriteLine("digite outro numero");
+    Console.ResetColor();
+}
+else{
+
+    while (contador <= 10)
+    {
+     int resultado = contador * numero;   
+    Console.WriteLine($"{numero} x {contador++} = {resultado}");
+    }
+    Console.WriteLine("  ");
+}
+    
